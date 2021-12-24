824f5188d079a9d9ad016255d7822de44d41e2fc
annamalai-sam <annamalai.capitalsam@gmail.com>
2021-12-24 06:15:46 +0530

    bug fix

--- assets/js/status.js
+++ assets/js/status.js
@@ -22,12 +22,12 @@ function output (mail){
         const user = apply_status[i];
         const gmail = user.Email;
         if (mail == gmail){
-            let result = `<tr>
+            let result = ` <table class="result"> <tr>
              <th class="row"> Name </th> <th class="row"> Date Of Birth </th> <th class="row"> Father's Name </th> <th class="row"> Email </th> <th class="row"> Result </th>
             </tr>
             <tr> 
             <td class="row">${user.First_Name} ${user.Last_name}</td> <td class="row"> ${user.DOB} </td><td class="row">  ${user.Par_name}</td> <td class="row"> ${user.Email}</td> <td class="row"> ${user.status} </td>
-            </tr>`;
+            </tr> </table> `;
 
             document.getElementById("output").innerHTML = result;
              console.log(gmail);
