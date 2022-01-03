2e7930afde80afba6c7f394c46d2550e9ef91ce5
annamalai-sam <annamalai.capitalsam@gmail.com>
2022-01-03 15:01:10 +0530

    push

--- assets/js/admin-login.js
+++ assets/js/admin-login.js
@@ -1,3 +1,19 @@
+let all_adm = [
+    {
+        "ad_mail":"chitra.muthukumaran@freshworks.com",  
+        "ad_pass":"FWE1019C",
+    },
+    {
+      "ad_mail":"prasannabharati.ram@freshworks.com",  
+      "ad_pass":"FWE1457C",
+    },
+    {
+        "ad_mail":"shanmuga.anandaraman@freshworks.com",  
+        "ad_pass":"FWE0580C",
+    },
+]
+localStorage.setItem("allAdmin", JSON.stringify(all_adm));
+
 let all_admin = JSON.parse(localStorage.getItem("allAdmin"));
 function login (){
     event.preventDefault();
