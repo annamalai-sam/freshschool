f5afeab1fdc954c776df7c02d3c1f590f5f635b8
annamalai-sam <annamalai.capitalsam@gmail.com>
2022-01-18 14:38:54 +0530

    push

--- assets/js/status.js
+++ assets/js/status.js
@@ -11,13 +11,13 @@ function output (mail){
     let apply_status = JSON.parse(localStorage.getItem("All_form"));
     for (let i of apply_status) {
         const user = apply_status[i];
-        const gmail = user.Email;
+        const gmail = i.Email;
         if (mail == gmail){
             let result = ` <table class="result"> <tr>
              <th class="row"> Name </th> <th class="row"> Date Of Birth </th> <th class="row"> Father's Name </th> <th class="row"> Email </th> <th class="row"> Result </th>
             </tr>
             <tr> 
-            <td class="row">${user.First_Name} ${user.Last_name}</td> <td class="row"> ${user.DOB} </td><td class="row">  ${user.Par_name}</td> <td class="row"> ${user.Email}</td> <td class="row"> ${user.status} </td>
+            <td class="row">${i.First_Name} ${i.Last_name}</td> <td class="row"> ${i.DOB} </td><td class="row">  ${i.Par_name}</td> <td class="row"> ${i.Email}</td> <td class="row"> ${i.status} </td>
             </tr> </table> `;
             document.getElementById("output").innerHTML = result;
             exist = false;
